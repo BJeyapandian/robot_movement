23a3c56556805b66abf9fc9193afc3e3ad3812cf
BJeyapandian <141110784+BJeyapandian@users.noreply.github.com>
2025-03-12 13:18:32 +0530

    Update terrain.py

--- robot_movements/terrain.py
+++ robot_movements/terrain.py
@@ -1,4 +1,8 @@
 from robot_movements.robot import Robot
+import logging
+
+
+logging.basicConfig(level=logging.INFO)  
 
 class Terrain:
     def __init__(self, grid_size=10):
@@ -7,23 +11,23 @@ class Terrain:
 
     def add_robot(self, robot_id):
         if robot_id in self.robots:
-            print("Error: Robot ID exists.")
+            logging.error("Error: Robot ID exists.")
             return
         self.robots[robot_id] = Robot(robot_id, self.grid_size)
-        print(f"Robot {robot_id} added at (0,0).")
+        logging.info(f"Robot {robot_id} added at (0,0).")
 
     def move_robot(self, robot_id, command):
         if robot_id not in self.robots:
-            print("Error: Robot ID not found.")
+            logging.error("Error: Robot ID not found.")  
             return
         occupied_positions = {robot.get_position() for rid, robot in self.robots.items() if rid != robot_id}
         try:
             self.robots[robot_id].move(command, occupied_positions)
         except ValueError as e:
-            print(f"Error: {e}")
+            logging.error(f"Error: {e}")  
 
     def get_robot_position(self, robot_id):
         if robot_id not in self.robots:
-            print("Error: Robot ID not found.")
+            logging.error("Error: Robot ID not found.")  
             return None
         return self.robots[robot_id].get_position()
